a7d55a9beae48a0ee588058faa645b6fe76313a5
Oxana Matveyuk <oxana@mishina.com>
2017-04-05 18:26:33 +0000

    Solved problem with circle linked list

--- Hackbright_whiteboard_hard/josephus.py
+++ Hackbright_whiteboard_hard/josephus.py
@@ -31,34 +31,84 @@ As a sanity case, if never skip anyone, the last person will be our survivor:
     10
 
 """
-# class Node(obj):
-#     """Class Node"""
-#     def __init__
+class Node(object):
+    """Class Node"""
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
 
-# class circle_ll(obj):
-#     """Class Circle Linked Linked"""
-#     def __init__(self, node=None):
-#         head = node
+    def __repr__(self):
+        return "<Node data={0}>".format(self.data)
+
+class Circle_ll(object):
+    """Class Circle Linked Linked"""
+    def __init__(self):
+        self.head = None
+
+    def insert(self, node):
+        if self.head == None:
+            self.head = node
+            node.next = self.head
+        else:
+            curr = self.head
+            while curr.next != self.head:
+                curr=curr.next
+            node.next = curr.next
+            curr.next = node
+
+    def remove(self, data):
+        # data is in head and circle linked list has only 1 node.
+        # Remove head. Empty circle linked list"
+        if self.head.data == data and self.head.next == self.head:
+            self.head = None
+            return
+
+        curr = self.head
+        # data is in head. Remove node which is head, move head to the next node
+        if self.head.data == data:
+            new_head = self.head.next
+            while curr.next != self.head:
+                curr = curr.next
+            self.head = new_head
+            curr.next = self.head
+        else:
+        # data is in inner node. Remove this node
+            while curr.data != data:
+                prev = curr
+                curr = curr.next
+            prev.next = curr.next
+
+    def display(self):
+        if self.head == None:
+            print "Empty circle linked llst"
+        else:
+            curr = self.head
+            while curr.next != self.head:
+                print curr.data,
+                curr = curr.next
+            print curr.data
+            print curr.next.data, '...'
 
 def find_survivor(num_people, kill_every):
     """Given num_people in circle, kill [kill_every]th person, return survivor."""
-    lst = [i for i in xrange(1, num_people+1)]
-    print lst
-    curr = -1
-    count = 0
-    while count <= num_people-1:
-        curr += kill_every
-        print "curr: ", curr
-        if curr >= num_people:
-            curr = curr - num_people + 1
-        print "curr: ", curr 
-        if lst[curr] != 0:
-            lst[curr] = 0
-            count += 1
-        print "count: ", count
+
+    # Create circle linked list with size = num.people
+    my_circle_ll = Circle_ll()
     for i in xrange(num_people):
-        if lst[i] != 0:
-            return lst[i]
+        my_circle_ll.insert(Node(i+1))
+
+    # Remve each kill_every node until circle linked list will have 1 node
+    curr = my_circle_ll.head
+    while curr != curr.next:
+        for i in xrange(kill_every - 1):
+            curr = curr.next
+        my_circle_ll.remove(curr.data)
+        curr = curr.next
+
+    return my_circle_ll.head.data
+       
+
+
 
 
 if __name__ == '__main__':
