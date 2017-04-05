ad5ea4257d63d650017d84ecd31efe3638aa8fe8
Oxana Matveyuk <oxana@mishina.com>
2017-04-05 18:37:13 +0000

    Adde Hackbright solution for josephus problem:

--- Hackbright_whiteboard_hard/josephus.py
+++ Hackbright_whiteboard_hard/josephus.py
@@ -97,18 +97,86 @@ def find_survivor(num_people, kill_every):
     for i in xrange(num_people):
         my_circle_ll.insert(Node(i+1))
 
-    # Remve each kill_every node until circle linked list will have 1 node
+    # Remove each kill_every node until circle linked list will have 1 node
     curr = my_circle_ll.head
     while curr != curr.next:
         for i in xrange(kill_every - 1):
+            # If we will every 3rd person, we'll skip over two
             curr = curr.next
         my_circle_ll.remove(curr.data)
         curr = curr.next
 
     return my_circle_ll.head.data
+
+# Solution Hackbright
+# class Node(object):
+#     """Doubly-linked node."""
+
+#     def __init__(self, data, prev=None, next=None):
+#         self.data = data
+#         self.prev = prev
+#         self.next = next
+
+#     def __repr__(self):
+#         return "<Node prev=%s data=%s next=%s>" % (
+#             self.prev.data, self.data, self.next.data)
+
+#     @classmethod
+#     def make_list(cls, n):
+#         """Construct a circular doubly-linked list of n items. Returns head node.
+
+#             >>> node = Node.make_list(3)
+
+#             >>> node.data
+#             1
+
+#             >>> node.next.data
+#             2
+
+#             >>> node.next.next.next.data
+#             1
+
+#             >>> node.prev.data
+#             3
+
+#             >>> node.prev.prev.prev.data
+#             1
+#         """
+
+#         # Make the first node (and remember that it's the first)
+#         first = node = prev = cls(1)
+
+#         # Make every other node
+#         for i in range(2, n + 1):
+#             node = Node(i, prev=prev)
+#             prev.next = node
+#             prev = node
+
+#         # Fix the last and first node's prev/next
+#         node.next = first
+#         first.prev = node
+
+#         return first
+
+# def find_survivor(num_people, kill_every):
+#     """Given num_people in circle, kill [kill_every]th person, return survivor."""
+#     node = Node.make_list(num_people)
+
+#     # Loop until we're the only item in the list (last survivor)
+#     while node.next != node:
+
+#         for i in range(kill_every - 1):
+#             # If we will every 3rd person, we'll skip over two
+#             node = node.next
+
+#         # We're on the node to kill. Remove it from our doubly-linked list
+#         node.prev.next = node.next
+#         node.next.prev = node.prev
+
+#         node = node.next
+
+#     return node.data
        
-
-
 
 
 if __name__ == '__main__':
