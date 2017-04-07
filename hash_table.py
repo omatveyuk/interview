17ab12c62d65df01c0e8bbf62d16f212c534db30
Oxana Matveyuk <oxana@mishina.com>
2017-04-07 07:39:50 +0000

    Review Hash Table implementation

--- hash_table.py
+++ hash_table.py
@@ -1,4 +1,25 @@
-"""Create hash table as list of lists
+""" Hash table implementation
+    >>> ht = HashTable()
+    >>> ht.set('a', 1)
+    >>> ht.set('aa', 2)
+    >>> ht.set('aaa', 3)
+    >>> ht.display()
+    { 'aaa' : 3 , 'a' : 1 , 'aa' : 2 , }
+    >>> ht.get('aaa')
+    3
+
+    >>> ht = HashTable()
+    >>> ht.set('b', 98)
+    >>> ht.set('bb', 196)
+    >>> ht.set('bbb', 294)
+    >>> ht.set('abc', 294)
+    >>> ht.display()
+    { 'bbb' : 294 , 'abc' : 294 , 'b' : 98 , 'bb' : 196 , }
+    >>> ht.get('bbb')
+    294
+    >>> ht.get('abc')
+    294
+
 
     
 """
@@ -7,31 +28,41 @@ class KeyValue(object):
         self.key = key
         self.value = value
 
+
 class HashTable(object):
     SIZE = 256
     def __init__(self):
         # implement hash table as list of lists to solve problem with colision
-        # We say that our internal list is a list of buckets, where each bucket
-        # holds all the key-value pairs that map to a particular index
+        # Our internal list is a list of buckets, where each bucket
+        # holds all the key, value pairs that map to a particular index
         self.hashlst = [[] for i in xrange(self.SIZE)]
-
 
     def set(self, key, value):
         """Put pair key, value in Hash Table with index from hash(key)"""
-        self.index_hashlst = _hash(key)
-        self.hashlst[index_hashlst].append((key, value))
+        index_hashlst = self._hash(key)
+        self.hashlst[index_hashlst].append(KeyValue(key, value))
 
     def get(self, key):
-        """Get pair key, value from Hash Table by hash(key)"""
-        self.index_hashlst = _hash(key)
+        """Get pair value from Hash Table by hash(key)"""
+        index_hashlst = self._hash(key)
         for item in self.hashlst[index_hashlst]:
-            if item[0] == key:
-                return self.hashlst[index_hashlst]
+            if item.key == key:
+                return item.value
 
     def _hash(self, key):
+        """ Calculate index in Hash Table """
         total = sum([ord(char) for char in key])
         return total % self.SIZE
 
     def display(self):
-        print self.hashlst
+        print "{",
+        for item in self.hashlst:
+            for each in item:
+                print "'"+each.key+"'",":",each.value,",",
+        print "}"
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. GREAT!!!! ***\n"
 
