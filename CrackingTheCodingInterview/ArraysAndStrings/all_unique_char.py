8e0b4fe68addd8541f803b8f1b6fa78002ba2100
Oxana Matveyuk <oxana@mishina.com>
2017-05-02 17:00:34 +0000

    CCI. ArraysAndStrings. Check if string has all unique chars.

---
+++ CrackingTheCodingInterview/ArraysAndStrings/all_unique_char.py
@@ -0,0 +1,28 @@
+"""Is all chars in string is unique?
+    Implement an algorithm to detemine if a string has all unique characters
+    without using additional data structures.
+"""
+def allUniqueChar(string):
+    """Check if all chars in string is unique.
+        Return: True if all chars is unique, othrwise False.
+        >>> allUniqueChar("Oxana")
+        False
+        >>> allUniqueChar("Misha")
+        True
+
+    """
+    # Create list with length 128, bc ASCII is 128 chars
+    letters = ['' for i in xrange(128)]
+    for char in string:
+        if letters[ord(char)] != '':
+            return False
+        letters[ord(char)] = char
+    return True
+
+if __name__ == '__main__':
+    import doctest
+    
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED.  all_unique_char.py  WORKS SUCCESSFULLY! ***\n"
+
+
